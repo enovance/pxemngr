c421ad06f78b127794c684cb574fe8c6e62ef5dc
Frederic Lepied <frederic.lepied@enovance.com>
2013-10-09 22:40:16 +0200

    finish multiple system name support

--- pxe/views.py
+++ pxe/views.py
@@ -17,16 +17,16 @@ import pxeparse
 
 def get_system(request, mac):
     try:
-        return System.objects.get(macaddress__mac=simplify_mac(mac))
-    except System.DoesNotExist:
+        return System.objects.filter(macaddress__mac=simplify_mac(mac))[0]
+    except IndexError:
         pass
 
     addr = request.META['REMOTE_ADDR']
     l = map(lambda x: '%02x' % int(x), addr.split('.'))
     for i in range(len(l), 1, -1):
         try:
-            return System.objects.get(macaddress__mac=''.join(l[0:i]))
-        except System.DoesNotExist:
+            return System.objects.filter(macaddress__mac=''.join(l[0:i]))[0]
+        except IndexError:
             pass
     print 'No system defined for %s (%s)' % (mac, addr)
     raise Http404
@@ -35,14 +35,24 @@ def localboot1(request):
     return localboot(request, get_mac(request))
     
 def localboot(request, mac):
-    try:
-        system = System.objects.get(macaddress__mac=simplify_mac(mac))
-        set_next_boot(system, settings.PXE_LOCAL)
-    except System.DoesNotExist:
-        fn = '%s/%s' % (settings.PXE_ROOT, mac2filename(simplify_mac(mac)))
-        create_symlink('%s/%s%s' % (settings.PXE_PROFILES, settings.PXE_LOCAL, settings.PXE_SUFFIX), fn)
+    return nextboot(request, mac, settings.PXE_LOCAL)
+
+def nextboot1(request, boot_name):
+    print 'nextboot1', boot_name
+    return nextboot(request, get_mac(request), boot_name)
     
-    return HttpResponse("Next boot set to local", mimetype="text/plain")
+def nextboot(request, mac, boot_name):
+    print 'nextboot', mac, boot_name
+    systems = System.objects.filter(macaddress__mac=simplify_mac(mac))
+    if len(systems) > 1:
+        set_next_boot(systems[0], boot_name)
+    else:
+        fn = '%s/%s' % (settings.PXE_ROOT, mac2filename(simplify_mac(mac)))
+        create_symlink('%s/%s%s' % (settings.PXE_PROFILES, boot_name,
+                                    settings.PXE_SUFFIX),
+                       fn)
+    return HttpResponse("Next boot set to %s" % boot_name,
+                        mimetype="text/plain")
 
 def profile1(request):
     return profile(request, get_mac(request))
